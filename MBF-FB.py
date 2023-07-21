4636fe1628b64a540c613206114ea2b96d90efe3
DARKEST OWL HACKERS <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-07-21 22:20:34 +0100

    Update MBF-FB.py

--- MBF-FB.py
+++ MBF-FB.py
@@ -2,6 +2,6 @@ import os, sys
 try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
 except:pass
 try:
-    __import__("multi").login_lagi334()
+    __import__("multi").login()
 except Exception as e:
     exit(str(e))
